7cca5961f86a8146e9a9ea332c4817cda8e310ab
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-03-16 07:28:09 +0000

    added required code to https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_deliver/_config.php
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_delivery/trunk@960 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -5,8 +5,17 @@
  * developed by www.sunnysideup.co.nz
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
+Director::addRules(50, array(
+	'pickupordeliverymodifier/$Action/$ID/$OtherID' => 'PickUpOrDeliveryModifier_AjaxController'
+));
+
+
 
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START ecommerce_delivery MODULE ----------------===================
+// *** PICK UP OR DELIVERY
+//NOTE: add http://svn.gpmd.net/svn/open/multiselectfield/tags/0.2/ for nicer interface
+//PickUpOrDeliveryModifier::set_form_header("Delivery Option (REQUIRED)");
+//ProductsAndGroupsModelAdmin::add_managed_model("PickUpOrDeliveryModifierOptions");
 //===================---------------- END ecommerce_delivery  MODULE ----------------===================
