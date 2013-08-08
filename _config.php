61df4db7b087918c5873c65300296f5866e19f8d
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-08-08 00:00:51 +0000

    MERGING Mark Guinnn\'s work: upgrading to SS3. THANK YOU MARK
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_delivery/trunk@4667 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -7,9 +7,24 @@
 **/
 
 
-
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_delivery MODULE ----------------===================
+//===================---------------- START ecommerce_delivery_electronically MODULE ----------------===================
+//MUST SET
+//ElectronicDelivery_OrderLog::set_random_folder_name_character_count(12);
+//ElectronicDelivery_OrderLog::set_add_htaccess_file(true);
+//ElectronicDelivery_OrderLog::set_files_to_be_excluded(array());
+//ElectronicDelivery_OrderLog::set_permissions_on_folder("0755");
+//ElectronicDelivery_OrderLog::set_order_dir("downloads");
+//===================---------------- END ecommerce_delivery_electronically  MODULE ----------------===================
+/*
 
-//NOTE: add http://svn.gpmd.net/svn/open/multiselectfield/tags/0.2/ for nicer interface
-//===================---------------- END ecommerce_delivery  MODULE ----------------===================
+dont forget to add the following .htaccess file to your originating folders:
+
+IndexIgnore *
+order allow,deny
+deny from all
+
+so that no-one can access the original files.
+
+
+*/
